2442f5ab4d39211b68c30f361209e1cbb31d619c
Phassarun Iamampai <phassarun@gmail.com>
2019-01-09 15:45:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 
 from src.comparison import compare_csv_files, create_csv_from_dataframe
-from src.csv_files_downloader import download_csv_files
+from src.csv_files_downloader import download_csv_files, get_prefix_list
 
 
 def report(df_1, df_2, store_name):
@@ -27,18 +27,25 @@ if __name__ == "__main__":
     export_folder = 'export'
     update_folder = 'updated'
     bucket_name = 'pricetrolley-prod_scraper'
-    prefix = 'redmart'
-    list_index = [-1, -2] # -1 is current file, -2 is past file
 
-    path_csv_files = download_csv_files(bucket_name, prefix, list_index, export_folder)
-    df_updated = compare_csv_files(path_csv_files[0], path_csv_files[1])
-    
-    # create updated csv
-    path_output_csv = '{}/{}'.format(update_folder, prefix)
-    create_csv_from_dataframe(df_updated, path_output_csv, suffix='updated')
+    columns = ['store_name', 'diff (%)', 'updated', 'original']
+    df_output = pd.DataFrame(columns=columns)
 
-    # create report csv
-    print(path_csv_files[0])
-    df_original = pd.read_csv(path_csv_files[0])
-    df_report = report(df_original, df_updated, store_name=prefix)
-    create_csv_from_dataframe(df_report, path_output_csv, suffix='report')
+    prefix_list = get_prefix_list(bucket_name)
+    for prefix in prefix_list:
+        list_index = [-1, -2] # -1 is current file, -2 is past file
+
+        path_csv_files = download_csv_files(bucket_name, prefix, list_index, export_folder)
+        df_updated = compare_csv_files(path_csv_files[0], path_csv_files[1])
+        
+        # create updated csv
+        path_output_csv = '{}/{}'.format(update_folder, prefix)
+        create_csv_from_dataframe(df_updated, path_output_csv, suffix='updated')
+
+        # create report csv
+        print(path_csv_files[0])
+        df_original = pd.read_csv(path_csv_files[0])
+        df_report = report(df_original, df_updated, store_name=prefix)
+        df_output = pd.concat([df_output, df_report])
+
+    df_output.to_csv('stores-report.csv')
